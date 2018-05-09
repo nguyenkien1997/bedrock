072d8617046db347d9b0727064a1199866f8d992
Paul McLanahan <pmac@mozilla.com>
2018-05-09 14:45:55 -0400

    Fix pulling images using :latest

--- docker/bin/build_images.sh
+++ docker/bin/build_images.sh
@@ -2,14 +2,17 @@
 
 set -exo pipefail
 
+# set all of the GIT environment variables
 source docker/bin/set_git_env_vars.sh
+
+# pull latest images
+docker pull mozorg/bedrock:latest
+docker pull mozorg/bedrock_test:latest
+docker pull mozorg/bedrock_assets:latest
 
 # get legal-docs
 git submodule sync
 git submodule update --init --recursive
-
-# pull latest images
-docker-compose pull app assets release
 
 # build fresh based on local changes
 docker-compose build release
